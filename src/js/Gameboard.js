6a383b2e4e36302eb6f541020e04c23286f65c91
Manpreet Singh <mskhurana729@gmail.com>
2025-07-16 18:05:01 -0400

    feat: Create placeShip function
    This function will place ships on gameboard and will keep track of those using keepTrack function

--- src/js/Gameboard.js
+++ src/js/Gameboard.js
@@ -2,7 +2,9 @@ class GameBoard {
   constructor(size = 10) {
     this.size = size;
     this.coordinates = {};
-    this.createGameBoard;
+    this.occupiedCoordinates = {};
+    this.keepTrack = {};
+    this.createGameBoard();
   }
   createGameBoard() {
     let size = this.size;
@@ -11,6 +13,46 @@ class GameBoard {
         this.coordinates[[i, j]] = true;
       }
     }
+  }
+  /**
+   * so we want to create a method which will place ships to coordinates of gameBoard
+   * Ship has a specific length and we can either place it horizontally or vertically consecutively not diagonally.
+   * we can not place 2 ships on one coordinate which means ships cannot overlap or go over-bound which means out of chess board coordinates
+   *
+   * so to make this function we will make this function take 3 arguments which are 1 ship instance ,second starting coordinate and third horizontally or vertically.
+   *
+   *    then we will make a new variable named length and will initialize it to ship.length
+   *
+   * then we will check for weather it is horizontal or vertical
+   *    if horizontal we will add coordinates starting from startingCoordinate and will exceed x coordinate with 1 for length times.
+   *
+   * and if vertical we will do the same but with y axis.
+   *
+   *
+   *
+   */
+  placeShip(ship, startingCoordinate, direction) {
+    let length = ship.length;
+    let [x, y] = startingCoordinate;
+    console.log(x, y);
+
+    console.log(!this.occupiedCoordinates[[x, y]] && this.coordinates[[x, y]]);
+    for (let i = 0; i < length; i++) {
+      if (!this.occupiedCoordinates[[x, y]] && this.coordinates[[x, y]]) {
+        console.log("hello from inside");
+        this.occupiedCoordinates[[x, y]] = true;
+        this.keepTrack[[x, y]] = ship;
+        if (direction === "horizontal") {
+          ++x;
+        } else {
+          ++y;
+        }
+      } else {
+        return "Please choose available coordinates";
+      }
+    }
+    console.log(this.keepTrack);
+    return this.keepTrack;
   }
 }
 module.exports = GameBoard;
