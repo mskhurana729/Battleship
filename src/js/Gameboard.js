8e790c893320abe134bdacddf2b831e9f0eeac1f
Manpreet Singh <mskhurana729@gmail.com>
2025-07-15 20:48:34 -0400

    feat: Create class Gameboard
    This class has size, coordinates as instance variables and createGameBoard method which creates an 2d array of length size*size.

---
+++ src/js/Gameboard.js
@@ -0,0 +1,15 @@
+class Gameboard {
+  constructor(size = 10) {
+    this.size = size;
+    this.coordinates = {};
+    this.createGameBoard;
+  }
+  createGameBoard() {
+    let size = this.size;
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        this.coordinates[[i, j]] = true;
+      }
+    }
+  }
+}
